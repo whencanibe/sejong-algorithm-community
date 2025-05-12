f84ce38b688445ba252c7f8701451f42c9072371
lseo98 <shlshl3088@naver.com>
2025-05-12 23:15:23 +0900

    마이페이지 추가가

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,13 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Ranking from './pages/Ranking';
+import MyPage from './pages/MyPage';
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/ranking" element={<Ranking />} />
+        <Route path="/mypage" element={<MyPage />} />
       </Routes>
     </Router>
   );
