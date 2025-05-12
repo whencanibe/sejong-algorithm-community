82bbec9b043eb24ed1b18350fe764a0be4b33355
lilchille <eonyakcho@gmail.com>
2025-05-12 15:33:07 +0900

    Prisma 세팅

---
+++ server/src/models/prisma.js
@@ -0,0 +1,13 @@
+import { PrismaClient } from '@prisma/client';
+
+const prisma =
+  global.prisma || // 이미 만들어 뒀으면 재사용
+  new PrismaClient({
+    log: ['query', 'error', 'warn'],   // 옵션 예시
+  });
+
+if (process.env.NODE_ENV !== 'production') {
+  global.prisma = prisma;              // dev에서 핫리로드 중복 방지
+}
+
+export default prisma;
