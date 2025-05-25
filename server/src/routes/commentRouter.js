b09b9dca7c609fb2030b4c73b6c09775aefd3e6d
bumfercarrr <bumfercar@gmail.com>
2025-05-25 22:25:10 +0900

    middleware 댓글,게시글 추가, client에 withCredentials 추가 안되어있음

--- server/src/routes/commentRouter.js
+++ server/src/routes/commentRouter.js
@@ -5,11 +5,12 @@ import {
   updateComment,
   deleteComment,
 } from '../controllers/commentController.js';
+import { isLoggedIn } from '../middlewares/middleware.js';
 
 const router = express.Router();
 
 router.get('/:postId', getCommentsByPostId);
-router.post('/:postId', createComment);
+router.post('/:postId', isLoggedIn, createComment);
 router.put('/:id', updateComment);
 router.delete('/:id', deleteComment);
 
